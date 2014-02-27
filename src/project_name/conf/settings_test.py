7ff35e128234f14c129abff5e5ab2b654a6eb15a
adelandgraaf <devnull@localhost>
2014-02-27 12:03:40 +0100

    * LEEROY^WDJANGO JENKINS!

--- src/project_name/conf/settings_test.py
+++ src/project_name/conf/settings_test.py
@@ -24,6 +24,10 @@ DATABASES = {
     }
 }
 
+INSTALLED_APPS += [
+    'django_jenkins',
+]
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
