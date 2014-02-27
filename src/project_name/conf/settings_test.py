e25def4ed50b78925cd8b61ebf01df55485f03a8
adelandgraaf <devnull@localhost>
2014-02-27 12:29:27 +0100

    * adding default jenkins tasks

--- src/project_name/conf/settings_test.py
+++ src/project_name/conf/settings_test.py
@@ -28,6 +28,13 @@ INSTALLED_APPS += [
     'django_jenkins',
 ]
 
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pylint',
+    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.django_tests',
+    'django_jenkins.tasks.run_pep8',
+)
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
