875febac48809ca29fcb249d29d3218a80f89911
Sergei Maertens <sergei@maykinmedia.nl>
2016-09-05 15:22:39 +0200

    Setup watchers in gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,34 +1,68 @@
 'use strict';
 
-var path = require('path');
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var bourbon = require('bourbon');
+var path = require('path');
+var rename = require("gulp-rename");
+var watch = require('gulp-watch');
+
 var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var neat = require('bourbon-neat');
 
 
+/**
+ * Sass related tasks
+ */
 var sass_src = 'src/{{ project_name|lower }}/sass/**/*.scss';
 var css_dir = 'src/{{ project_name|lower }}/static/{{ project_name|lower }}/css';
 
 
 gulp.task('sass', function() {
     gulp.src(sass_src)
+        .pipe(sourcemaps.init())
         .pipe(sass({
-            // sourceMap: true, // requires extra plugin
             outputStyle: 'expanded',
-            includePaths: [
-                path.join(__dirname, 'node_modules/bootstrap/scss')
-            ].concat(bourbon.includePaths)
+            includePaths: neat.includePaths
         }).on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(css_dir));
 });
 
 
-// watch task
-gulp.task('default',function() {
+gulp.task('watch_sass', ['sass'], function() {
     gulp.watch(sass_src, ['sass']);
 });
+
+
+/**
+ * Collectstatic equivalent (more or less)
+ */
+
+var STATIC_ROOT = './static';
+var STATIC_APPDIRS = 'src/{{ project_name|lower }}/*/static/**/*.js';
+var STATICFILES_DIRS = [
+    'src/{{ project_name|lower }}/static/'
+];
+
+gulp.task('watch_collectstatic', function() {
+    var src = [STATIC_APPDIRS].concat(STATICFILES_DIRS);
+    return watch(src, {verbose: true, base: 'src/{{ project_name|lower }}'})
+        .pipe(rename(function(file) {
+            var bits = file.dirname.split('/');
+            // strip off 'appname' and 'static', if present
+            file.dirname = bits.slice(2).join('/');
+            return file;
+        }))
+        .pipe(gulp.dest(STATIC_ROOT));
+});
+
+
+/**
+ * default executed task when running 'gulp'
+ */
+gulp.task('default', ['watch_sass', 'watch_collectstatic']);
