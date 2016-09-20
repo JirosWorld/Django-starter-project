26ca3835c8f47a055caf183e695a90d6e533f5ad
Alex de Landgraaf <alex@maykinmedia.nl>
2016-09-20 14:30:35 +0200

    Noticed there are alternatives to this in the frontend branch

--- run_jenkins_py3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-virtualenv env --python=python3
-npm install --production
-./env/bin/python bootstrap.py test
-. env/bin/activate
-./env/bin/python src/manage.py collectstatic --link --noinput
-./env/bin/python src/manage.py jenkins --settings={{ project_name }}.conf.test --project-apps-tests  --enable-coverage --pep8-ignore=W293,W291,E501,E261 --pep8-exclude=migrations,static,media --pylint-rcfile=pylint.rc
