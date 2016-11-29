e67b6505ee3c592501522caeb7d6420bc9516576
Sergei Maertens <sergei@maykinmedia.nl>
2016-11-29 18:45:38 +0100

    Fix little mistakes

--- src/project_name/conf/base.py
+++ src/project_name/conf/base.py
@@ -94,7 +94,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'myproject.utils.context_processors.settings',
+                '{{ project_name|lower }}.utils.context_processors.settings',
             ],
             'loaders': RAW_TEMPLATE_LOADERS
         },
