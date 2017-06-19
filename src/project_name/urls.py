b61ce667d44f45560c7c40ecdad39e7e77f2ecd3
Joeri Bekker <joeri@maykinmedia.nl>
2017-06-19 13:13:13 +0200

    Place hijack under the admin package (purely cosmetic)

--- src/project_name/urls.py
+++ src/project_name/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     # url(r'^admin_tools/', include('admin_tools.urls')),
     url(r'^admin/password_reset/$', auth_views.password_reset, name='admin_password_reset'),
     url(r'^admin/password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^admin/hijack/', include('hijack.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
-    url(r'^hijack/', include('hijack.urls')),
 
     # Simply show the master template.
     url(r'^$', TemplateView.as_view(template_name='demo.html')),
