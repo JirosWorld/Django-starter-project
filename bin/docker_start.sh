997314662e0fab3c7061fcbaaf01a6c3e9e6b8a6
Sergei Maertens <sergei@maykinmedia.nl>
2018-11-16 15:25:18 +0100

    :whale: Refactored Dockerfile & docker scripts
    * File rewritten to use dedicated stages for backend/frontend build and
      actual production image
    * `docker_start.sh` modified to remove collectstatic and support
      concurrency out of the box (in uwsgi)
    * `docker build` now runs collectstatic, delivering a more ready
      build artifact
    * virtualenv removed - docker is already an isolated environment
    * Python 3.7 (Alpine) as base image
    * Added helper script to manage building/releasing the image

--- bin/docker_start.sh
+++ bin/docker_start.sh
@@ -15,14 +15,18 @@ done
 
 >&2 echo "Database is up."
 
-# Collect static files
->&2 echo "Collect static files"
-python src/manage.py collectstatic --noinput
-
 # Apply database migrations
 >&2 echo "Apply database migrations"
 python src/manage.py migrate
 
 # Start server
 >&2 echo "Starting server"
-uwsgi --http :8000 --module {{ project_name|lower }}.wsgi --static-map /static=/app/static --chdir=src
+uwsgi \
+    --http :8000 \
+    --module {{ project_name|lower }}.wsgi \
+    --static-map /static=/app/static \
+    --static-map /media=/app/media  \
+    --chdir src \
+    --processes 2 \
+    --threads 2
+    # processes & threads are needed for concurrency without nginx sitting inbetween
