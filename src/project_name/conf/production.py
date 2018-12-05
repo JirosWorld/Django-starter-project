4a45e55c49329dcb3854bf54f2d1d56950576b7c
Alex de Landgraaf <alex@maykinmedia.nl>
2018-12-05 10:59:01 +0100

    Ensuring we by default use SAMEORIGIN, DENY can be activated if required

--- src/project_name/conf/production.py
+++ src/project_name/conf/production.py
@@ -86,7 +86,10 @@ SHOW_ALERT = False
 SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_HTTPONLY = True
 CSRF_COOKIE_SECURE = True
-X_FRAME_OPTIONS = 'DENY'
+
+# SAMEORIGIN by default. Enable the following if no iframes are used
+# X_FRAME_OPTIONS = 'DENY'
+
 # Only set this when we're behind Nginx as configured in our example-deployment
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_CONTENT_TYPE_NOSNIFF = True # Sets X-Content-Type-Options: nosniff
--- src/project_name/conf/staging.py
+++ src/project_name/conf/staging.py
@@ -80,7 +80,9 @@ ENVIRONMENT = 'staging'
 SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_HTTPONLY = True
 CSRF_COOKIE_SECURE = True
-X_FRAME_OPTIONS = 'DENY'
+
+# SAMEORIGIN by default. Enable the following if no iframes are used
+# X_FRAME_OPTIONS = 'DENY'
 # Only set this when we're behind Nginx as configured in our example-deployment
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_CONTENT_TYPE_NOSNIFF = True # Sets X-Content-Type-Options: nosniff
