c64221520326e16cb4e00a6732e840d4fd0362b0
Sven van de Scheur <svenvandescheur@gmail.com>
2019-03-06 11:24:32 +0100

    :bulb: - Document {% capture %} {% placekitten %} and {% placekitten_src %}

--- src/project_name/utils/templatetags/utils.py
+++ src/project_name/utils/templatetags/utils.py
@@ -17,6 +17,15 @@ class CaptureNode(template.Node):
 
 @register.tag
 def capture(parser, token):
+    """
+    Captures contents and assigns them to variable.
+    Allows capturing templatetags that don't support "as".
+
+    Example:
+
+        {% capture as body %}{% lorem 20 w random %}{% endcapture %}
+        {% include 'components/text/text.html' with body=body only %}
+    """
     args = token.split_contents()
     if len(args) < 3 or args[-2] != 'as':
         raise template.TemplateSyntaxError("'capture' tag requires a variable name after keyword 'as'.")
@@ -28,9 +37,26 @@ def capture(parser, token):
 
 @register.simple_tag
 def placekitten(width=800, height=600):
+    """
+    Renders a "placekitten" placeholder image.
+
+    Example:
+
+        {% placekitten %}
+        {% placekiiten 200 200 %}
+    """
     return format_html('<img src="{}" />'.format(placekitten_src(width, height)))
 
 
 @register.simple_tag
 def placekitten_src(width=800, height=600):
+    """
+    Return a "placekitten" placeholder image url.
+
+    Example:
+
+        {% placekitten_src as src %}
+        {% placekitten_src 200 200 as mobile_src %}
+        {% include 'components/image/image.html' with mobile_src=mobile_src src=src alt='placekitten' only %}
+    """
     return '//placekitten.com/{}/{}'.format(width, height)
