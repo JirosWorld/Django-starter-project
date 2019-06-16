c2906b96e308117c0aa5d05bdd48ae26a26e4c67
Alex de Landgraaf <alex@maykinmedia.nl>
2019-06-16 14:24:00 +0200

    Removing newrelic from default-project

--- src/project_name/wsgi.py
+++ src/project_name/wsgi.py
@@ -10,18 +10,4 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-
-def init_newrelic():
-    if os.environ.get('PROJECT_ROOT'):
-        try:
-            import newrelic.agent
-            newrelic.agent.initialize(os.path.join(os.environ.get('PROJECT_ROOT'), 'newrelic.ini'), 'production')
-        except Exception as e:
-            print("Could not initialize New Relic APM, ignoring:")
-            print(e)
-
-
-# Enable New Relic on production
-# init_newrelic()
-
 application = get_wsgi_application()
