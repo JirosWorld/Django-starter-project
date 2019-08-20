62871387407a52821025ceba9dd101fbc6aab9e6
Alex de Landgraaf <alex@maykinmedia.nl>
2019-08-20 13:55:21 +0200

    Switching to sql instead of sh file

--- docker-init-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRESQL_USERNAME" --dbname "$POSTGRESQL_DATABASE" <<-EOSQL
-         CREATE DATABASE {{ project_name|lower }};
-         GRANT ALL PRIVILEGES ON DATABASE {{ project_name|lower }} TO $POSTGRESQL_USERNAME;
-EOSQL
-
