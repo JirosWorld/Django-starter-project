049b5fa17b3e6535ac3eb01b0809c98c780706d3
Joeri Bekker <joeri@maykinmedia.nl>
2019-08-28 10:05:19 +0000

    :wrench: Use database connection for 60 seconds instead of closing them after every request.

--- src/project_name/conf/production.py
+++ src/project_name/conf/production.py
@@ -20,6 +20,7 @@ DATABASES = {
         'PASSWORD': '{{ project_name|lower }}',
         'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',  # Set to empty string for default.
+        'CONN_MAX_AGE': 60,  # Lifetime of a database connection for performance.
     }
 }
 
