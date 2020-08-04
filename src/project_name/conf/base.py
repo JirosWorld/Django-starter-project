93a42a9187c9bf08ed72aa1dff9eae582f6e5376
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2020-08-04 14:02:58 +0200

    Remove unneeded line from TEMPLATES.OPTIONS.context_processors

--- src/project_name/conf/base.py
+++ src/project_name/conf/base.py
@@ -103,8 +103,6 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "{{ project_name|lower }}.utils.context_processors.settings",
-                # REQUIRED FOR ADMIN INDEX
-                "django_admin_index.context_processors.dashboard",
             ],
             "loaders": RAW_TEMPLATE_LOADERS,
         },
