c0b665d47ebb1a885a0f6140d3020c8085d4a704
Sergei Maertens <sergei@maykinmedia.nl>
2020-10-28 17:53:04 +0100

    :sparkles: Add convenience function to pull config from the env

---
+++ src/project_name/conf/includes/environ.py
@@ -0,0 +1,20 @@
+from decouple import Csv, config as _config, undefined
+
+
+def config(option: str, default=undefined, *args, **kwargs):
+    """
+    Pull a config parameter from the environment.
+
+    Read the config variable ``option``. If it's optional, use the ``default`` value.
+    Input is automatically cast to the correct type, where the type is derived from the
+    default value if possible.
+
+    Pass ``split=True`` to split the comma-separated input into a list.
+    """
+    if "split" in kwargs:
+        kwargs.pop("split")
+        kwargs["cast"] = Csv()
+
+    if default is not undefined and default is not None:
+        kwargs.setdefault("cast", type(default))
+    return _config(option, default=default, *args, **kwargs)
