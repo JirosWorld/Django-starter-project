beea29337e3167758b8ebd5f679aa85c52465665
Dario Mory <dario@mory.dev>
2021-11-12 14:26:10 +0100

    :ok_hand: Process PR feedback

--- src/project_name/accounts/tests/factories.py
+++ src/project_name/accounts/tests/factories.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.utils.timezone import now
 
 import factory
 from factory.django import DjangoModelFactory
@@ -8,18 +7,18 @@ User = get_user_model()
 
 
 class UserFactory(DjangoModelFactory):
-    username = factory.Faker("username")
+    username = factory.Sequence(lambda n: f"user-{n}")
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
-    email = factory.Sequence(
-        lambda n: "test{1}{0}@maykinmedia.nl".format(now().timestamp(), n)
-    )
     password = factory.PostGenerationMethodCall("set_password", "password")
 
     class Meta:
         model = User
 
 
-class SuperUserFactory(UserFactory):
+class StaffUserFactory(UserFactory):
     is_staff = True
+
+
+class SuperUserFactory(StaffUserFactory):
     is_superuser = True
